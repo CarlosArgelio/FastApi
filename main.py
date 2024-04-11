0e260b96ec50d97dcf4c8c46a32ef463f0a6fa22
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-04-11 18:55:51 -0400

    feat: add method get

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 app.title = "My app backend with FastAPI"
 app.version = "0.0.1"
+app.description = "This is a simple app backend with FastAPI"
+app.contact = {
+    "name": "Carlos Argelio Palacios Ramos",
+    "url": "https://www.linkedin.com/in/palaciosrcarlosa/",
+    "email": "carlosargelio0104@gmail.com",
+}
+
+app.debug = True # False default ( This need enviroment configuration )
+
+movies = [
+    {
+		"id": 1,
+		"title": "Avatar",
+		"overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+		"year": "2009",
+		"rating": 7.8,
+		"category": "AcciÃ³n"
+	}
+]
 
 @app.get('/', tags=['home'])
 def message():
-    return "Hello world!"
+    return HTMLResponse('<h1>Hello world</h1>')
+
+@app.get('/movies', tags=['movies'])
+def get_movies():
+    return movies
