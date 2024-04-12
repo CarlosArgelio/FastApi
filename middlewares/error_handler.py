bdfa7457ca8f026e0e4a05cb7b7ef70b67e119eb
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-04-11 23:21:05 -0400

    fix

--- middlewares/error_handler.py
+++ middlewares/error_handler.py
@@ -1,3 +1,4 @@
+from typing import Union
 from starlette.middleware.base import BaseHTTPMiddleware
 from fastapi import FastAPI, Request, Response
 from fastapi.responses import JSONResponse
@@ -9,7 +10,8 @@ class ErrorHandler(BaseHTTPMiddleware):
     def __init__(self, app: FastAPI) -> None:
         super().__init__(app)
     
-    async def dispatch(self, request: Request, call_next) -> Response | JSONResponse:
+    # fix
+    async def dispatch(self, request: Request, call_next):
         try:
             return await call_next(request)
         except Exception as e:
